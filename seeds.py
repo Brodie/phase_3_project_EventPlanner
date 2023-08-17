275579b799f3a06cae99413bd010cfc25a720164
Brodie Ashcraft <brodiea19@gmail.com>
2023-08-17 10:37:33 -0700

    successfully seeds db with minimal test data

--- seeds.py
+++ seeds.py
@@ -4,6 +4,7 @@ from models import User, Event
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from faker import Faker
+import random
 
 fake = Faker()
 
@@ -12,7 +13,26 @@ engine = create_engine("sqlite:///EventPlanner.db")
 Session = sessionmaker(bind=engine)
 session = Session()
 
-for i in range(5):
-    user = User(name=fake.unique.name())
-    session.add(user)
-    session.commit()
+EVENTS = ["wedding", "birthday", "expo", "concert", "corporate", "other"]
+
+
+# creating methods to seed db
+def delete_entries():
+    session.query(User).delete()
+    session.query(Event).delete()
+
+
+def seed_db():
+    delete_entries()
+    for i in range(5):
+        user = User(name=fake.unique.name())
+        session.add(user)
+        session.commit()
+
+        event = Event(title=random.choice(EVENTS), owner_id=user.id)
+        session.add(event)
+        session.commit()
+
+
+# running methods to seed db
+seed_db()
