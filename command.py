fb722c26fc4575b88428445d724f2decb5b026b8
Brodie Ashcraft <brodiea19@gmail.com>
2023-08-19 15:06:33 -0700

    began building CLI application

--- command.py
+++ command.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import User, Event, Invite
+from simple_term_menu import TerminalMenu
+from cli_color_py import red, green, yellow, cyan
 
 
 class CommandLine:
@@ -8,4 +10,11 @@ class CommandLine:
         self.current_user = None
 
     def start(self):
-        pass
+        self.clear()
+        options = ["Login", "Sign-Up"]
+        menu = TerminalMenu(options)
+        entry_index = menu.show()
+        print(cyan(f"selected {options[entry_index]}"))
+
+    def clear(self):
+        print("\n" * 50)
